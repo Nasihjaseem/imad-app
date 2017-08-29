55517d55000aa67ab536bf7b685a34f4a12bb795
Nasih jazeem <Nasihjaseem@users.noreply.github.com>
2017-08-29 12:27:43 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 
 var button=document.getElementById('counter');
 var counter=0;
-button.onclick= function(){
+button.onclick = function(){
 
 
-    counter=counter + 1;
-    var count=document.getElementById('inc');
-    count.innerHTML= counter.toString();
+    counter = counter + 1;
+    var count = document.getElementById('inc');
+    count.innerHTML = counter.toString();
 };
