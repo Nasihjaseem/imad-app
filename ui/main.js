77d3adce52d07b6c88381ded695478f34da5b84e
Nasih jazeem <Nasihjaseem@users.noreply.github.com>
2017-08-29 12:34:38 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@
 var demo=document.getElementById("counter");
 var counter=0;
 demo.onclick=  function() {
-counter=counter+1;
+counter=counter + 1;
     document.getElementById("inc").innerHTML = counter.toString();
 }
 };
