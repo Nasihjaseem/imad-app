9678beda848861c5cfb9156f840d9325b482e469
Nasih jazeem <Nasihjaseem@users.noreply.github.com>
2017-08-29 15:39:55 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,13 @@
 
 var button=document.getElementById("butn");
 button.onclick=  function() {
-    var request= new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            if(request.status===200){
-            var counter = request.responseText;
-            var span = document.getElementById("inc");
-            span.innerHTML = counter.toString();
-             console.log(counter);
-            }
-        }
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       document.getElementById("inc").innerHTML = xhttp.responseText;
     }
+};
+          
    
 };
