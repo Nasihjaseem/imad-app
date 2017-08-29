f38081b414878bc9be8b40599324483d7e649a03
Nasih jazeem <Nasihjaseem@users.noreply.github.com>
2017-08-29 13:10:13 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,13 @@
 
-var demo=document.getElementById("butn");
-var counter=0;
-demo.onclick=  function() {
-counter=counter+1;
+var button=document.getElementById("butn");
+button.onclick=  function() {
+    var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.onreadystate===XMLHttpRequest.DONE){
+            if(request.status===200){
+            var counter=request.responseText;
+            }
+        }
+    }
     document.getElementById("inc").innerHTML = counter.toString();
 }
