4c56466b09195c86f7a3dc73b722b754b5c16981
Nasih jazeem <Nasihjaseem@users.noreply.github.com>
2017-08-30 03:55:08 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/Cars.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Cars.html'));
 });
 app.get('/profile.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Cars.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
 app.get('/Cars/audi.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/cars', 'audi.html'));
