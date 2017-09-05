58285e6b4dfaec1448ffa077b404e91a1eac5a4d
Nasih jazeem <Nasihjaseem@users.noreply.github.com>
2017-09-05 15:17:32 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ submit.onclick=function(){
     {
         list+='<li>'+ names[i] +'<li>';
     }
-    var ul=document.getElementById("nameList");
-    ul.innerHTML=list;
+document.getElementById("nameList").innerHTML=list;
+
     console.log(list);
 };
